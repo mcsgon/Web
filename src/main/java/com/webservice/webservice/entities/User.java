634ff757f1e19a4c191ae40fe5a6775cecf34969
mcsgon <mairacarolinasaraiva@gmail.com>
2024-02-11 21:37:45 -0300

    UserResource

--- src/main/java/com/webservice/webservice/entities/User.java
+++ src/main/java/com/webservice/webservice/entities/User.java
@@ -2,26 +2,36 @@ package com.webservice.webservice.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 @Entity
-@Table (name = "tb_user")
+@Table (name ="users")
 public class User implements Serializable{
     private static final long serialVersionUID=1L;
-
-
+    
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
+    
+    public User() {
+	}
+
+
     public User(Long id, String name, String email, String phone, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
         this.password = password;
-    }
-    public static long getSerialversionuid() {
-        return serialVersionUID;
+    
     }
     public Long getId() {
         return id;
