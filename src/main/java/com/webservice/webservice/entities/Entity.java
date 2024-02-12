df83ec5f6e5d8e502a700523753a2adfd545356d
mcsgon <mairacarolinasaraiva@gmail.com>
2024-02-11 21:06:50 -0300

    aula1

---
+++ src/main/java/com/webservice/webservice/entities/Entity.java
@@ -0,0 +1,5 @@
+package com.webservice.webservice.entities;
+
+public @interface Entity {
+
+}
---
+++ src/main/java/com/webservice/webservice/entities/Table.java
@@ -0,0 +1,7 @@
+package com.webservice.webservice.entities;
+
+public @interface Table {
+
+    String name();
+
+}
